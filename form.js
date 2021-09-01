fc0d4db668a3768af53f9ce61e9a2d80aaef3f97
April Yax <83990078+aprilyax@users.noreply.github.com>
2021-09-01 10:08:01 -0700

    initial add: to do list files

---
+++ form.js
@@ -0,0 +1,23 @@
+// user input - includes validation
+function TodoForm({addTodo}){
+  const [value,setValue] = React.useState('');
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  // Add Todo user text-input box
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add Your To-Do..."
+        onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
---
+++ todo.js
@@ -0,0 +1,10 @@
+
+// items on the to-do list
+function Todo({todo,index,remove}){
+  function handle(){
+    console.log('Ping:',index);
+    remove(index);
+  }
+  return <div className="todo" onClick={handle}>{todo.text} (-)</div>
+  
+}
